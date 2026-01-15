f8b5e4f38f32723c490cf2d9b6748e5db66a5e1c
Emanuel Taborda <emanuel.cristotaborda@gmail.com>
2026-01-14 22:57:29 -0300

    Initial commit - Classes Atividade e Participante

---
+++ src/test/java/com/curso_dev_superior/desafio2/Desafio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_dev_superior.desafio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
